6bb88670e438bf34e17d7a7f381ff4d8132e9b6d
Miguel Augusto <miguelaugustoabsn@gmail.com>
2023-04-13 20:19:29 -0300

    commit26

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Header } from './componentes/Header';
+import { Post } from './componentes/Post';
+
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from './componentes/Sidebar';
+
+function App() {
+    return (
+    <div>
+      <Header />
+
+      <div className={ styles.wrapper} />
+        <Sidebar /> 
+        <main>
+          <Post 
+            author="Miguel Augusto" 
+            content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, sed accusantium adipisci obcaecati fuga laborum quam blanditiis reiciendis necessitatibus mollitia. A, quos quidem! Tempore quae et veniam, cum minus ad!"
+        />
+        <Post
+            author="Miguel Nascimento"
+            content="Um novo post muito legal!"
+        />
+        </main>
+    </div>
+
+    )
+
+}
+
+
